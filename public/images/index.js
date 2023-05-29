ae05ef21c6c1db54aa761bafc7044694e26c8f43
Damiano Schirinzi <damianoschirinzi.dev@gmail.com>
2023-05-29 17:41:48 +0100

    Initial Commit

---
+++ public/images/index.js
@@ -0,0 +1,39 @@
+import BackgroundOverlay_1 from "../images/BG_Overlay_2.png";
+import BackgroundOverlay_2 from "../images/BG_Overlay_2.png";
+import Chef_1 from "../images/Chef.jpg";
+import Chef_2 from "../images/Chef2.jpg";
+import Chef_3 from "../images/Chef3.jpg";
+import Food_1 from "../images/Food.png";
+import Food_2 from "../images/food.jpg";
+import Food_3 from "../images/food 2.jpg";
+import Food_4 from "../images/food 3.jpg";
+import Food_5 from "../images/food 5.jpg";
+import Food_6 from "../images/Hummus-Mediterranean-Food.jpg";
+import Logo from "../images/logo.png";
+
+export {
+  BackgroundOverlay_1,
+  BackgroundOverlay_2,
+  Chef_1,
+  Chef_2,
+  Chef_3,
+  Food_1,
+  Food_2,
+  Food_3,
+  Food_4,
+  Food_5,
+  Food_6,
+  Logo
+};
+
+/*export const Chef_1 = require('../images/BG_Overlay_2.png')
+export const Chef_2 = require('../images/BG_Overlay_2.png')
+export const Chef_3 = require('../images/BG_Overlay_2.png')
+export const Food_1 = require('../images/BG_Overlay_2.png')
+export const Food_2 = require('../images/BG_Overlay_2.png')
+export const Food_3 = require('../images/BG_Overlay_2.png')
+export const Food_4 = require('../images/BG_Overlay_2.png')
+export const Food_5 = require('../images/BG_Overlay_2.png')
+export const Food_6 = require('../images/BG_Overlay_2.png')
+export const Food_7 = require('../images/BG_Overlay_2.png')
+export const Logo = require('../images/BG_Overlay_2.png') */
